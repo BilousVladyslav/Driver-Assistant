af66f2ebffcc4bd446353f308ef2507285cd7483
Vladyslav Bilous <vladyslav.bilous@stemsc.com>
2020-12-06 18:14:32 +0200

    Created home page, profile, registration, login, navigation and footer components.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/l10n/l18n.ts
@@ -0,0 +1,210 @@
+export const i18nAsset = {
+  'en-US': {
+    changeLocale: 'Change language',
+    home: {
+      title: 'Home',
+      about_first: 'The ability to communicate with vehicles in the flow, ie informing drivers about the presence of an unusual situation on the road or the approach of special vehicles is a very important topic of everyday life.',
+      about_second: 'Also very common is the problem of accidents involving motorcyclists. According to statistics, 1 million people die in road accidents in the world every year. This brings great moral and material damage. Of these, 22% of accidents are caused by events involving motor vehicles.',
+      about_thirth: 'A software system to optimize communication with cars could enable drivers to monitor car traffic data in real time and reduce the likelihood of accidents while driving and at intersections by installing sensors in the car and at traffic lights that would signal the vehicle in in the case of approaching a traffic light.',
+      about_fourth: 'The software system would be able to do so by obtaining data on the location of vehicles, the condition of traffic lights and the distance from the car to the nearest facilities.',
+      about_fifth: 'And the reliability of the software system could provide the user with the needs of their own safety while driving.'
+    },
+    login: {
+      title: 'Login',
+      subtitle: 'Login',
+      username: {
+        title: 'Username',
+        required: 'Login is a required'
+      },
+      password: {
+        title: 'Password',
+        required: 'Password is a required',
+      },
+      submit: 'Sign in',
+      close: 'Close',
+      register: {
+        question: 'New here',
+        action: 'Register'
+      },
+      wrong_data: 'Wrong data'
+    },
+    navbar: {
+      logout: 'Log out',
+      profile: 'Profile',
+      home: 'Home'
+    },
+    register: {
+      title: 'New user registration',
+      subtitle: 'Enter your personal data',
+      email: {
+        placeholder: 'Enter your e-mail',
+        required: 'Email is',
+        strong: 'required'
+      },
+      username: {
+        placeholder: 'Username',
+        required: 'Username is a required',
+        min_length: 'Username must be a minimum of 5 characters',
+      },
+      password: {
+        placeholder: 'Password',
+        required: 'Password is a required',
+        min_length: 'Password must be a minimum of 8 characters',
+        max_length: 'Password requires maximum 40 symbols',
+        regex: 'Password must have uppercase and lowercase Latin letters, digit and character'
+      },
+      password_confirm: {
+        placeholder: 'Confirm the password',
+        required: 'Password confirming is a required',
+        must_match: 'Passwords doesn\'t mutch'
+      },
+      first_name: {
+        placeholder: 'First name',
+        required: 'First name is a required',
+        max_length: 'First name requires maximum 50 symbols',
+        regex: 'First name must consists of uppercase and lowercase Latin letters'
+      },
+      last_name: {
+        placeholder: 'Last name',
+        required: 'Last name is a required',
+        max_length: 'Last name requires maximum 50 symbols',
+        regex: 'Last name must consists of uppercase and lowercase Latin letters'
+      },
+      dialog: {
+        title: 'Registration completed!',
+        description: 'Registration completed successfully. Now you shoud activate yopur profile. To do this, visit link what we`ve sent to your e-mail.',
+        close: 'Close'
+      },
+      submit: 'Submit'
+    },
+    profile: {
+      title: 'Personal information',
+      passwords_title: 'Change password',
+      email_title: 'Change Email',
+      email: 'E-mail',
+      new_email: {
+        placeholder: 'Enter your new e-mail',
+        required: 'Email is',
+        strong: 'required'
+      },
+      username: 'Username',
+      first_name: {
+        placeholder: 'First name',
+        required: 'First name is a required',
+        max_length: 'First name requires maximum 50 symbols',
+        regex: 'First name must consists of uppercase and lowercase Latin letters'
+      },
+      last_name: {
+        placeholder: 'Last name',
+        required: 'Last name is a required',
+        max_length: 'Last name requires maximum 50 symbols',
+        regex: 'Last name must consists of uppercase and lowercase Latin letters'
+      },
+      save: 'Save changes'
+    },
+  },
+  'uk-UA': {
+    changeLocale: 'Змінити мову',
+    home: {
+      title: 'Головна',
+      about_first: 'Можливість комунікації з транспортними засобами в потоці, тобто інформування водіїв про наявність незвичайної ситуації на дорозі або про наближення спецтранспорту є досить актуальною темою повсякдення. ',
+      about_second: 'Також дуже поширеною є проблема виникнення ДТП з участю мотоциклістів . За статистикою в світі з 1 рік від ДТП помирає 1 млн людей. Це приносить великий моральний і матеріальний збиток. З них 22% ДТП припадає саме на події за участю мототранспорту.',
+      about_thirth: 'Програмна система для оптимізації комунікації з автомобілями змогла б надати можливість водіям слідкувати за даними щодо руху автомобілів у режимі реального часу та зменшити ймовірність виникнення ДТП під час руху і на перехрестях за рахунок установки датчиків в автомобілі та на світлофори, які б подавали сигнал транспортному засобу у разі наближення до світлофора.',
+      about_fourth: 'Програмна система мала б таку можливість, отримуючи дані щодо місцерозташування транспортних засобів, стану світлофорів та відстанню від автомобіля до найближчих об’єктів.',
+      about_fifth: 'А надійність програмної системи могла б забезпечити користувача у потребах власної безпеки під час руху на транспортному засобі. '
+    },
+    login: {
+      title: 'Авторизація',
+      subtitle: 'Авторизуватися',
+      username: {
+        title: 'Ім\'я користувача',
+        required: 'Введіть логін'
+      },
+      password: {
+        title: 'Пароль',
+        required: 'Введіть пароль',
+      },
+      submit: 'Увійти',
+      close: 'Закрити',
+      register: {
+        question: 'Вперше тут',
+        action: 'Зареєструватися'
+      },
+      wrong_data: 'Невірні дані'
+    },
+    register: {
+      title: 'Реєстрація нового користувача',
+      subtitle: 'Введіть ваші дані',
+      email: {
+        placeholder: 'Введіть вашу пошту',
+        required: 'Електронна пошта',
+        strong: 'обов\'язкова'
+      },
+      username: {
+        placeholder: 'Логін',
+        required: 'Логін обов\'язковий',
+        min_length: 'Логін має складати мінімум 5 символів',
+      },
+      password: {
+        placeholder: 'Пароль',
+        required: 'Пароль обов\'язковий',
+        min_length: 'Пароль має складати мінімум 8 символів',
+        max_length: 'Пароль може складати максимум 40 букв',
+        regex: 'Пароль має містити букви верхнього та нижнього регістрів, цифри та символи'
+      },
+      password_confirm: {
+        placeholder: 'Підтвердіть пароль',
+        required: 'Підтвердження паролю обов\'язкове',
+        must_match: 'Паролі не співпадають'
+      },
+      first_name: {
+        placeholder: 'Ім\'я',
+        required: 'Ім\'я обов\'язкове',
+        max_length: 'Ім\'я може складати максимум 50 букв',
+        regex: 'Ім\'я має складати великі та малі латинські букви'
+      },
+      last_name: {
+        placeholder: 'Прізвище',
+        required: 'Прізвище обов\'язкове',
+        max_length: 'Прізвище може складати максимум 50 букв',
+        regex: 'Прізвище має складати великі та малі латинські букви'
+      },
+      dialog: {
+        title: 'Реєстрація завершена!',
+        description: 'Реєстрація успішно завершена. Зараз Ви маєта активувати Ваш профіль. Для цього, перейдіть по посиланню, яке ми вислали Вам на електронну пошту.',
+        close: 'Закрити'
+      },
+      submit: 'Підтвердити'
+    },
+    navbar: {
+      logout: 'Вийти',
+      profile: 'Профіль',
+      home: 'Головна'
+    },
+    profile: {
+      title: 'Персональна інформація',
+      passwords_title: 'Змінити пароль',
+      email_title: 'Змінити пошту',
+      email: 'Пошта',
+      username: 'Логін',
+      new_email: {
+        placeholder: 'Введіть вашу нову пошту',
+        required: 'Пошта',
+        strong: 'обов\'язкова'
+      },
+      first_name: {
+        placeholder: 'Ім\'я',
+        required: 'Ім\'я обов\'язкове',
+        max_length: 'Ім\'я може складати максимум 50 букв',
+        regex: 'Ім\'я має складати великі та малі латинські букви'
+      },
+      last_name: {
+        placeholder: 'Прізвище',
+        required: 'Прізвище обов\'язкове',
+        max_length: 'Прізвище може складати максимум 50 букв',
+        regex: 'Прізвище має складати великі та малі латинські букви'
+      },
+      save: 'Зберегти зміни'
+    },
+  }
+};
